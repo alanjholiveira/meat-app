126dbcc36814915594727361d44eac5d75d02ad0
Alan Oliveira <alan.oliveira@basis.com.br>
2020-06-17 13:15:15 -0300

    Melhorias no compo radio, implementaçao da listagem de produto no order

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { RadionOption } from 'app/shared/radio/radio-option.model';
+import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { OrderService } from './order.service';
 
 @Component({
   selector: 'mt-order',
@@ -14,9 +16,27 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão Refeição', value: 'REG'}
   ]
 
-  constructor() { }
+  constructor(
+    private orderService: OrderService
+  ) { }
 
   ngOnInit() {
+  }
+
+  cartItems(): CartItem[]{
+    return this.orderService.cartItems();
+  }
+
+  increaseQty(item: CartItem) {
+    this.orderService.increaseQty(item);
+  }
+
+  decreaseQty(item: CartItem) {
+    this.orderService.decreaseQty(item);
+  }
+
+  remove(item: CartItem) {
+    this.orderService.remove(item);
   }
 
 }
