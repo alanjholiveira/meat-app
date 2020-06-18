6786faea222efcd510a3ecade79203c2752c0d6c
Alan Oliveira <alan.oliveira@basis.com.br>
2020-06-18 09:33:23 -0300

    Finalizaçao do carrinho de compra

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart-service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
+
+import { MEAT_API } from '../app.api';
 
 @Injectable()
 export class OrderService {
   
   constructor(
-    private cartService: ShoppingCartService
+    private cartService: ShoppingCartService,
+    private http: Http
   ){}
 
   itemsValue(): number {
@@ -28,6 +37,20 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.cartService.removeItem(item);
+  }
+
+  clear() {
+    this.cartService.clear();
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers  = new Headers();
+    headers.append('Content-Type', 'application/json')
+    return this.http.post(`${MEAT_API}/orders`,
+                          JSON.stringify(order),
+                          new RequestOptions({headers: headers}))
+                    .map(response => response.json())
+                    .map(order => order.id);
   }
 
 }
